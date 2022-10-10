1938a4e618d2b9714125acf5cb934da7d824d5fe
raselahamed2 <raselahamed7472@gmail.com>
2022-10-10 23:21:38 +0600

    error page and path

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
 // import logo from './logo.svg';
 import './App.css';
 import {createBrowserRouter, RouterProvider} from 'react-router-dom'
+import Main from './componts/Main/Main';
+import Topics from '../src/componts/Topics/Topics'
+import Statistics from '../src/componts/Statistics/Statistics'
+import Blog from '../src/componts/Blog/Blog'
+import ErrorPage from './componts/ErrorPage/ErrorPage';
 
 function App() {
   const router =  createBrowserRouter([
-
+    {
+      path: '/',
+      element: <Main></Main>,
+      errorElement: <ErrorPage></ErrorPage>,
+      children: [
+        {
+          path: '/',
+          element: <Topics></Topics>
+        },
+        {
+          path: '/statistics', 
+          element: <Statistics></Statistics>
+        },
+        {
+          path: '/log',
+          element: <Blog></Blog>
+        }
+      ]
+      
+    }
   ])
   return (
     <div className="App">
